882ef45b5b2ca7f8e26b5cdcaf623378d9d329f5
JuanFernandoCastaneda <jf.castaneda@uniandes.edu.co>
2019-02-11 12:32:32 -0500

    Creado el DTO de Producción

--- s1_foros-api/src/main/java/co/edu/uniandes/csw/foros/dtos/ProduccionDTO.java
+++ s1_foros-api/src/main/java/co/edu/uniandes/csw/foros/dtos/ProduccionDTO.java
@@ -44,48 +44,83 @@ public class ProduccionDTO implements Serializable {
     }
 
     /**
-     * Getter del id de una producción.
-     * @return 
+     * Getter del id de la producción.
+     * @return id de la producción.
      */
     public long darIdProduccion() {
         return idProduccion;
     }
 
-    public void setIdProduccion(long idProduccion) {
+    /**
+     * Setter del id de la producción.
+     * @param idProduccion nuevo id de la producción.
+     */
+    public void editarIdProduccion(long idProduccion) {
         this.idProduccion = idProduccion;
     }
 
-    public String getNombre() {
+    /**
+     * Getter del nombre de la producción.
+     * @return nombre de la producción.
+     */
+    public String darNombre() {
         return nombre;
     }
-
-    public void setNombre(String nombre) {
+    
+    /**
+     * Setter del nombre de la producción.
+     * @param nombre nuevo nombre de la producción.
+     */
+    public void editarNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public String getDescripcion() {
+    /**
+     * Getter de la descripción de la producción.
+     * @return nombre de la producción.
+     */
+    public String darDescripcion() {
         return descripcion;
     }
 
-    public void setDescripcion(String descripcion) {
+    /**
+     * Setter de la descripción de la producción.
+     * @param descripcion nueva descripción de la producción.
+     */
+    public void editarDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 
-    public ClasificacionAudiencia getClasificacionAudiencia() {
+    /**
+     * Getter de la clasificación de audiencia de la producción.
+     * @return clasificacionAudiencia clasificación de audiencia de la producción.
+     */
+    public ClasificacionAudiencia darClasificacionAudiencia() {
         return clasificacionAudiencia;
     }
 
+    /**
+     * Setter de la clasificación de audiencia de la producción.
+     * @param clasificacionAudiencia nueva clasificación de audiencia de la producción.
+     */
     public void setClasificacionAudiencia(ClasificacionAudiencia clasificacionAudiencia) {
         this.clasificacionAudiencia = clasificacionAudiencia;
     }
 
+    /**
+     * Getter de la calificación promedio de la producción.
+     * @return calificacionPromedio calificación promedio de la producción.
+     */
     public int getCalificacionPromedio() {
         return calificacionPromedio;
     }
 
+    /**
+     * Setter de la calificación promedio de la producción.
+     * @param calificacionPromedio nueva calificación promedio de la producción.
+     */
     public void setCalificacionPromedio(int calificacionPromedio) {
         this.calificacionPromedio = calificacionPromedio;
     }
-    
     
 }
